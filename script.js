de8cd95bbfcfa82701995c6d667dfb40f1856c87
Kh1one <98650259+Kh1one@users.noreply.github.com>
2022-08-12 21:21:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById('dogpic').addEventListener('click', function(){
+
+    alert('guk guk');
+});
+
+document.getElementById('wordsUnder').addEventListener('click', function(){
+
+    alert('gambarnya, bukan ini');
+});
+
+document.getElementById('nguli').addEventListener('click', function(){
+
+    alert('semangat nguli wkwkwk');
+});
